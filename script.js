32f3fd2f5c8efd864c304f7697b3f82e7f65ad58
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-11-04 02:28:11 +0100

    score ok

--- script.js
+++ script.js
@@ -15,20 +15,22 @@
 // genero un array di 16 bombe
 // devo far si che ogni elemento dell'array si accoppi a un elemento della griglia
 
-// I numeri nella lista delle bombe non possono essere duplicati.
-// In seguito l’utente clicca su ogni cella:
-// se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
-// altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// okI numeri nella lista delle bombe non possono essere duplicati.
+// okIn seguito l’utente clicca su ogni cella:
+// ok se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
+// ok altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
 // (come detto sull’effetiva interfaccia fate voi, non ci son specifiche vincolanti, ma partite semplici)
 // La partita termina quando il giocatore clicca su una bomba
 // o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve scoprire tutte le bombe e comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato un quadratino con un numero consentito. 
-// (quindi se ci pensate dovrete tenere traccia del punteggio).
+// ok(quindi se ci pensate dovrete tenere traccia del punteggio).
 
 const gridCont = document.getElementById("container");
 const btnEasy = document.getElementById("easy-btn");
 const btnMed = document.getElementById("med-btn");
 const btnHard = document.getElementById("hard-btn");
+const resultBox = document.getElementById("result");
+let score= 0;
 
 // selezionato il lv il click itera il ciclo for per il numero di elementi a seconda della difficoltà.
 // creandoli e inserendoli in griglia
@@ -38,28 +40,30 @@ btnEasy.addEventListener("click",
         // genero l'array di 16 bombe
         const newBomb = randNumUniqueGen ("1", "100", "16");
         console.log("newbomb", newBomb);
+
         for (let i = 1; i <= 100; i++) {
             let newElement = elementGenerator ("div" , "square-easy");
-            gridCont.append(newElement); 
+            gridCont.append(newElement);
             newElement.setAttribute("id", i); //settiamo un id univoco per ogni elemento
 
             newElement.addEventListener("click",
                 function() {
                     newElement.innerText= i;
                     newElement.id = i; //richiamiamo l'attributo settato a r.43
-                    console.log(newElement);
-                   let boom = parseInt(newElement.id);
-                   console.log(boom);
-                   
-                   
-                   if(newBomb.includes(boom)){      // verifico se l'array di bombe contiene i numeri uguali, se sono uguali esplodono
-                       this.classList.add("bomb");
-                       console.log("hai perso");
-                    
+                    let boom = parseInt(newElement.id);
+                        
+                    if(newBomb.includes(boom)){     //verifico se l'array di bombe contiene i numeri uguali, se sono uguali esplodono 
+                        this.classList.add("bomb");
+                        console.log("hai perso");
+                        resultBox.innerHTML = `Boom!!, GAME OVER, Hai totalizzato ${score} punti `;
+                        console.log(score);
+                        
                     } else {
                         this.classList.add("clicked-true");
-
+                        score += 1;
+                        console.log(score);
                     }
+        
                 }
             );
             
@@ -72,16 +76,34 @@ btnEasy.addEventListener("click",
 
 btnMed.addEventListener("click",
     function() {
+         // genero l'array di 16 bombe
+         const newBomb = randNumUniqueGen ("1", "81", "16");
+         console.log("newbomb", newBomb);
         for (let i = 1; i <= 81; i++) {
             let newElement = elementGenerator ("div" , "square-med");
+            gridCont.append(newElement);
+            newElement.setAttribute("id", i); //settiamo un id univoco per ogni elemento
 
             newElement.addEventListener("click",
                 function() {
-                    this.classList.add("clicked-true");
-                    newElement.append(i);  
+                    newElement.innerText= i;
+                    newElement.id = i; //richiamiamo l'attributo settato a r.43
+                    let boom = parseInt(newElement.id);
+                        
+                    if(newBomb.includes(boom)){     //verifico se l'array di bombe contiene i numeri uguali, se sono uguali esplodono 
+                        this.classList.add("bomb");
+                        console.log("hai perso");
+                        resultBox.innerHTML = `Boom!!, GAME OVER, Hai totalizzato ${score} punti `;
+                        console.log(score);
+                        
+                    } else {
+                        this.classList.add("clicked-true");
+                        score += 1;
+                        console.log(score);
+                    }
+        
                 }
             );
-            gridCont.append(newElement);  
                
         }
     }
@@ -89,16 +111,34 @@ btnMed.addEventListener("click",
 
 btnHard.addEventListener("click",
     function() {
+         // genero l'array di 16 bombe
+         const newBomb = randNumUniqueGen ("1", "49", "16");
+         console.log("newbomb", newBomb);
         for (let i = 1; i <= 49; i++) {
             let newElement = elementGenerator ("div" , "square-hard");
+            gridCont.append(newElement);
+            newElement.setAttribute("id", i); //settiamo un id univoco per ogni elemento
 
             newElement.addEventListener("click",
                 function() {
-                    this.classList.add("clicked-true");
-                    newElement.append(i);  
+                    newElement.innerText= i;
+                    newElement.id = i; //richiamiamo l'attributo settato a r.43
+                    let boom = parseInt(newElement.id);
+                        
+                    if(newBomb.includes(boom)){     //verifico se l'array di bombe contiene i numeri uguali, se sono uguali esplodono 
+                        this.classList.add("bomb");
+                        console.log("hai perso");
+                        resultBox.innerHTML = `Boom!!, GAME OVER, Hai totalizzato ${score} punti `;
+                        console.log(score);
+                        
+                    } else {
+                        this.classList.add("clicked-true");
+                        score += 1;
+                        console.log(score);
+                    }
+        
                 }
             );
-            gridCont.append(newElement);  
                  
         }
     }
